afba29c982a6a1c861249ab06100568ff3dcd5ca
maxencemachu <maxence.machu@akarah.com>
2021-07-21 23:27:15 +0200

    test(add): add test for online function

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,4 +1,4 @@
-import {callAPI, getWeatherStringFromCode} from '../src/functions';
+import {callAPI, getWeatherStringFromCode, renderOnline} from '../src/functions';
 
 describe('Weather string', () => {
     it('should return a string', () => {
@@ -6,6 +6,25 @@ describe('Weather string', () => {
     })
     it('should return a sunny', () => {
         expect(getWeatherStringFromCode(0)).toBe('SUNNY')
+    })
+})
+
+/*
+**Simulation du DOM**
+Jest est livré avec jsdom qui simule un environnement DOM comme si vous étiez dans le navigateur. 
+Cela signifie que chaque API du DOM que nous appelons peut être observée de la même manière qu'elle le serait dans un navigateur
+*/
+describe('DOM', () => {
+
+    document.body.innerHTML =
+    '<div>' +
+    '  <span id="js-online" />'+
+    '</div>';
+
+    it('should show offline', () => {  
+        renderOnline(false);
+
+        expect(document.getElementById('js-online').innerText).toEqual('Your are offline')
     })
 })
 
@@ -18,6 +37,6 @@ describe('API', () => {
 
         let weather = callAPI();
         expect(fetch).toHaveBeenCalledTimes(1);
-        expect(weather).toMatchObject({})
+        expect(typeof weather).toBe('object')
     })
 })
